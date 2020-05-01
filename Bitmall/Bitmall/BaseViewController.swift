6f944c5ff3c7ed7aa88b7fff60f2e685ca4d83ef
Carlos Pava <cpava@condorlabs.io>
2020-05-01 10:31:37 -0500

    add coordinator pattern

---
+++ Bitmall/Bitmall/BaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewController.swift
+//  Bitmall
+//
+//  Created by Carlos Pava on 1/05/20.
+//  Copyright © 2020 Carlos Pava. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
---
+++ Bitmall/Bitmall/Home/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  Bitmall
+//
+//  Created by Carlos Pava on 1/05/20.
+//  Copyright © 2020 Carlos Pava. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController {
+    weak var delegate: HomeViewControllerDelegate?
+}
+
