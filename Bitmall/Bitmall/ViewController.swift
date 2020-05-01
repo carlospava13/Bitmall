bb791958fcbfad5a603e7cf8c4a5d0b0b73e5dbd
Carlos Pava <cpava@condorlabs.io>
2020-05-01 10:11:32 -0500

    add project

---
+++ Bitmall/Bitmall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitmall
+//
+//  Created by Carlos Pava on 1/05/20.
+//  Copyright © 2020 Carlos Pava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
